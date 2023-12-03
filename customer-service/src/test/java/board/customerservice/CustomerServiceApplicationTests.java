e10a4f68016dc1d25f7cc7db5273ad69a95e60e1
Yasse7 <jerrariyasser2001@gmail.com>
2023-12-03 18:39:29 +0100

    all

---
+++ customer-service/src/test/java/board/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package board.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
